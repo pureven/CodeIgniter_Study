4ea3eb9f9b6296f710dc6d7ab51f62131efa5db8
VIC <392608770@qq.com>
2019-07-15 20:38:32 +0800

    IM: test source CodeIgniter!

---
+++ application/controllers/Test_default.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 39260
+ * Date: 2019/7/15
+ * Time: 19:26
+ */
+
+class Test_default extends CI_Controller
+{
+    public function index()
+    {
+        $this->load->view('test_default');
+    }
+
+}
